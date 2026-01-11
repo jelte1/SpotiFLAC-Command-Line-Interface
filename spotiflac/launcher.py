6e0bb49eefc1cc7dc366cca4d71e04f6d382323f
JBG <jbgamer17252@gmail.com>
2026-01-11 20:03:53 +0100

    - test

--- spotiflac/launcher.py
+++ spotiflac/launcher.py
@@ -3,16 +3,7 @@
 Launcher script for SpotiFLAC
 """
 
-import sys
-import os
 from spotiflac.SpotiFLAC import main
-
-if getattr(sys, 'frozen', False):
-    application_path = sys._MEIPASS
-else:
-    application_path = os.path.dirname(os.path.abspath(__file__))
-
-sys.path.insert(0, application_path)
 
 if __name__ == "__main__":
     main()
